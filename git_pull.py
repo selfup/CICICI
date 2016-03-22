354c9ba280f8de365fa6d9c8a1c958c0b33065a2
Regis <boudinot.regis@yahoo.com>
2016-03-22 12:08:57 -0600

    next step is a clone logic gate

--- git_pull.py
+++ git_pull.py
@@ -1,4 +1,9 @@
 import os, sys, time
 
+cicici_there = os.popen('ls').read()
+
+if "CICICI" not in cicici_there:
+    os.system('git clone https://github.com/selfup/CICICI')
+
 os.system('cd ~/some_app_file_path && git pull')
 time.sleep(5)
